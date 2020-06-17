05096dded1490dd94ffa6ccb4d57ceadd5a886a5
Jose Carlos Fernandez Rodriguez <jocafernanro@gmail.com>
2020-06-17 16:15:57 +0200

    project in progress

---
+++ src/plugins/vee-validate.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import { ValidationProvider, extend, setInteractionMode, localize } from 'vee-validate';
+import * as rules from 'vee-validate/dist/rules';
+import es from 'vee-validate/dist/locale/es.json';
+ 
+for (let rule in rules) {
+  // add the rule
+  extend(rule, rules[rule]);
+}
+ 
+localize('es', es);
+setInteractionMode('lazy'); // Para que la validación salte al abandonar el campo
+ 
+// Register it globally
+Vue.component('ValidationProvider', ValidationProvider);
